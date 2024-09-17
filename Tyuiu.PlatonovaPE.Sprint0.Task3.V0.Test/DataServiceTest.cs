82dd2bc8d5d1c8e0bb579cd53206eab46a5ba724
Полина Платонова <Tyuiu.PlatonovaPE@outlook.com>
2024-09-17 14:21:25 +0300

    Task3

---
+++ Tyuiu.PlatonovaPE.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PlatonovaPE.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.PlatonovaPE.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataServiceTest.Sum(5, 5));
+        }
+    }
+}
