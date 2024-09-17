5c9b42e2ce9ab21c073d2f5b358d3c4a05d44b78
Полина Платонова <Tyuiu.PlatonovaPE@outlook.com>
2024-09-17 10:31:25 +0300

    Task1

---
+++ Tyuiu.PlatonovaPE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PlatonovaPE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "ПОЛИНА";
+            int age = 17;
+            Console.WriteLine("Привет меня зовут " + name + "мне " + age + "лет" );
+            Console.ReadKey();
+        }
+    }
+}
